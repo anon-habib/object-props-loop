aab9b98c6ad033bc47a4201d65c0d9f9ebdf8d80
Habibur Rahman Ove <anon.haabib@gmail.com>
2023-09-15 22:32:38 +0600

    finding value in property

--- object-props-loop.js
+++ object-props-loop.js
@@ -1,3 +1,4 @@
+/* property, value accessing */
 let shoppingCart= {
     mango: '3kg',
     books: 3,
@@ -8,3 +9,15 @@ let shoppingCart= {
 }
 console.log(shoppingCart);
 
+// accessing an object property/key's value by dot notation
+let booksCount = shoppingCart.books
+console.log(booksCount);
+
+// accessing an object property/key's value in alternative 
+let computerCount = shoppingCart['computer'];
+console.log(computerCount);
+
+// accessing an object property/key's value by a variable
+let whichFish = 'fish'
+let myFish = shoppingCart[whichFish]; // this is the same as above but with variables instead of strings
+console.log(myFish);
